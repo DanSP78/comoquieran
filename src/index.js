f3a9e33e99b21bd7b7c45d716718155470c3e7dc
Daniel <salasdaniel686@gmail.com>
2023-03-25 22:44:03 -0600

    Prueba

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app"; //equivale al codigo que esta en app.js
 import './database';
 import { PUERTO } from "./config";
-app.listen(PUERTO);
-console.log("servidor en puerto", PUERTO);
+const port = process.env.PORT || PUERTO;
+app.listen(port);
+console.log("servidor en puerto", port);
